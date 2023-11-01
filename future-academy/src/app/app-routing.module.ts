fb20d0cb1f5c1d7c80bfe5788ec6afa71a09befd
Emmanuel <emmanuel012k@gmail.com>
2023-11-01 11:15:55 +0530

    Implemented corporate register form

--- future-academy/src/app/app-routing.module.ts
+++ future-academy/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { RegisterComponent } from './admin/register/register.component';
 import { FresherRegisterComponent } from './fresher/fresher-register/fresher-register.component';
 import { ExperienceRegisterComponent } from './experience/experience-register/experience-register.component';
 import { FreelancerRegisterComponent } from './freelancer/freelancer-register/freelancer-register.component';
+import { CorporateRegisterComponent } from './corporate/corporate-register/corporate-register.component';
  
 
 const routes: Routes = [
@@ -13,6 +14,7 @@ const routes: Routes = [
   { path: 'fresherregister', component:FresherRegisterComponent},
   { path: 'experienceregister', component:ExperienceRegisterComponent},
   { path: 'freelancerregister', component:FreelancerRegisterComponent},
+  { path: 'corporate', component:CorporateRegisterComponent},
 
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component:LoginComponent },
